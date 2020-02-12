8bb4913ed89ac09f59a897513b5987f5afd82f7d
Michael Harms <michael.harms6010@gmail.com>
2020-02-12 16:25:32 -0600

    adds alt tag

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ export default function About() {
         <div className="about-page">
             <div className="about-content">
                 <h1>About the site</h1>
-                <p>ZECpages is a social networking site for zcash users. "Take it easy" is the ZECpages motto. A good reason to swing by is if you want to chat and make new friends.</p>
+                <p>ZECpages is a public director of zcash users. "Take it easy" is the ZECpages motto. A good reason to swing by is if you want to chat and make new friends.</p>
             </div>
             <div className="about-content">
                 <h2>Want to join in? Don't have a z-address?</h2>
@@ -13,7 +13,7 @@ export default function About() {
             </div>
             <div className="about-content">
                 <h2>About the Creator</h2>
-                <img src={mike} />
+                <img alt="a silly picture of Mike" src={mike} />
                 <p>Hi! I'm Mike! I'm a friendly full-stack web developer interested in cryptocurrency, privacy, robots, carnivory, and piano. You can contact me on <a href="https://twitter.com/michaelharms70">Twitter</a> or via shielded ZEC memo: zs1q4jdaenhv5pdn4uqn86qekt7j2dch66uuszyp05ywne8z7yh56dhqjekpqle72skfl8ycd86ew4</p>
             </div>
          </div>
