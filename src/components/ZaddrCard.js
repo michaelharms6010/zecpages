137c4717458114b46931178f26b95ab3abd0aae2
Michael Harms <michael.harms6010@gmail.com>
2020-02-12 14:12:04 -0600

    adds and formats icons

--- src/components/ZaddrCard.js
+++ src/components/ZaddrCard.js
@@ -1,5 +1,11 @@
 import React, {useState, useEffect} from "react";
-import {copyTextToClipboard } from "../utils/copy"
+import {copyTextToClipboard } from "../utils/copy";
+import proofactive from "../icons/proof-active.png";
+import proofinactive from "../icons/proof-inactive.png";
+import twitteractive from "../icons/twitter-active.png";
+import twitterinactive from "../icons/twitter-inactive.png";
+import websiteactive from "../icons/website-active.png";
+import websiteinactive  from "../icons/website-inactive.png";
 
 export default function ZaddrCard ({user, copied, setCopied}) {
     const [httpsString, setHttpsString] = useState("");
@@ -26,9 +32,9 @@ export default function ZaddrCard ({user, copied, setCopied}) {
                 <button onClick={_ => handleCopy(user.zaddr, user.id)}>{user.id === copied ? "Copied!" : "Copy Zaddr"}</button>
             </div>
             <div className="card-bottom-row">
-                {user.proofposturl ? <a target="_new" href={user.proofposturl}>Proof</a> : null}
-                {user.website ? <a target="_new" href={`${httpsString}${user.website}`}>Website</a> : null}
-                {user.twitter ? <a target="_new" href={`https://twitter.com/${user.twitter}`}>Twitter: @{user.twitter}</a> : null}
+                {user.proofposturl ? <a target="_new" href={user.proofposturl}><img src={proofactive} /></a> : <img src={proofinactive} />}
+                {user.website ? <a target="_new" href={`${httpsString}${user.website}`}><img src={websiteactive} /></a> : <img src={websiteinactive} />}
+                {user.twitter ? <a target="_new" href={`https://twitter.com/${user.twitter}`}><img src={twitteractive} /></a> : <img src={twitterinactive} />}
                 {user.email ? <span>{user.email}</span> : null}
             </div>
         </div>
