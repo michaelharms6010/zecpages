ef8a611ba83b420649a6f5058dd3f2340ba771e3
Michael Harms <michael.harms6010@gmail.com>
2020-02-12 14:16:06 -0600

    adds email icon

--- src/components/ZaddrCard.js
+++ src/components/ZaddrCard.js
@@ -1,11 +1,14 @@
 import React, {useState, useEffect} from "react";
 import {copyTextToClipboard } from "../utils/copy";
+
 import proofactive from "../icons/proof-active.png";
 import proofinactive from "../icons/proof-inactive.png";
 import twitteractive from "../icons/twitter-active.png";
 import twitterinactive from "../icons/twitter-inactive.png";
 import websiteactive from "../icons/website-active.png";
 import websiteinactive  from "../icons/website-inactive.png";
+import emailactive from "../icons/email-active.png";
+import emailinactive from "../icons/email-inactive.png";
 
 export default function ZaddrCard ({user, copied, setCopied}) {
     const [httpsString, setHttpsString] = useState("");
@@ -35,7 +38,7 @@ export default function ZaddrCard ({user, copied, setCopied}) {
                 {user.proofposturl ? <a target="_new" href={user.proofposturl}><img src={proofactive} /></a> : <img src={proofinactive} />}
                 {user.website ? <a target="_new" href={`${httpsString}${user.website}`}><img src={websiteactive} /></a> : <img src={websiteinactive} />}
                 {user.twitter ? <a target="_new" href={`https://twitter.com/${user.twitter}`}><img src={twitteractive} /></a> : <img src={twitterinactive} />}
-                {user.email ? <span>{user.email}</span> : null}
+                {user.email ? <a href={`mailto:${user.email}`}><img src={emailactive} /></a> : <img src={emailinactive} />}
             </div>
         </div>
     )
