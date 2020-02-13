c8a5c8781bbd3f453e4c42e4731ddef649d8ba77
Michael Harms <michael.harms6010@gmail.com>
2020-02-12 23:25:57 -0600

    adds copy text

--- src/components/About.js
+++ src/components/About.js
@@ -12,13 +12,15 @@ export default function About() {
                 <h2>What is Zcash?</h2>
                 <p>Zcash is a decentralized means of exchange that employs Zero Knowledge Proofs to prevent printing transaction details onto its public ledger.</p>
                 <p>When transacting between z-addresses, the sender, receiver, amount, and memo are not shared with anyone else along the way!</p>
+                <p>Want a lil Zcash to take this ole blockchain for a spin? <a target="_new" href="https://twitter.com/zcashvr">Tweet at ZcashVR!</a></p>
                 <a target="_new" href="https://www.zecwallet.co/"><button className="cta">Get started in under a minute!</button></a>
             </div>
             <div className="about-content">
                 <h2>About the Creator</h2>
                 <img alt="silly picture of Mike" src={mike} />
-                <p>Hi! I'm Mike! I'm a friendly web developer interested in cryptocurrency, privacy, robots, carnivory, and piano. You can contact me on <a href="https://twitter.com/michaelharms70">Twitter</a> or via shielded ZEC memo:</p>
+                <p>Hi! I'm Mike! I'm a friendly web developer interested in human autonomy, the impending Golden Age, privacy, robots, carnivory, and piano. You can contact me on <a href="https://twitter.com/michaelharms70">Twitter</a> or via shielded ZEC memo:</p>
                 <p className="zaddr">zs1q4jdaenhv5pdn4uqn86qekt7j2dch66uuszyp05ywne8z7yh56dhqjekpqle72skfl8ycd86ew4</p>
+                <p>I'd love to hear about your experience using this app and any features or improvements you'd like to see!</p>
             </div>
          </div>
     )
