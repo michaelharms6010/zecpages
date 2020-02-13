5cb82cd70d8731123f7ec2e217236e235150f521
Michael Harms <michael.harms6010@gmail.com>
2020-02-13 11:02:05 -0600

    adds search field

--- src/components/ZaddrList.js
+++ src/components/ZaddrList.js
@@ -7,6 +7,8 @@ import logo from "../zcash-icon.png"
 export default function ZaddrList (props) {
     const { zaddrs, setZaddrs } = useContext(ZaddrContext);
     const [copied, setCopied] = useState(0);
+    const [results, setResults] = useState(zaddrs);
+    const [search, setSearch] = useState("");
 
     useEffect(_ => {
         axiosWithAuth().get("https://be.zecpages.com/users")
@@ -16,22 +18,54 @@ export default function ZaddrList (props) {
           .catch(err => console.error(err));
       },[])
 
+    useEffect( _ => {
+        setResults(zaddrs)
+    }, [zaddrs])
+
+
+    useEffect( _ => {
+        if (search) {
+            setResults(zaddrs.filter(item => 
+                {
+                    if (item.twitter){
+                        return item.username.includes(search) || item.twitter.includes(search)
+                    } else {
+                        return item.username.includes(search)
+                    }
+                }
+            ))
+        } else {
+            setResults(zaddrs)
+        }
+
+    }, [search])
+
     return(
         <div className="zaddr-list">
             <h2>ZECpages: Open-Source Zaddr Directory</h2>
             
+           
             {zaddrs.length > 0 
-            ? zaddrs.map(item => 
+            ? 
+            <>
+            <label>Search:</label>
+             <input
+             className="search-input"
+            value={search}
+            onChange={e => setSearch(e.target.value)}
+            />
+            {results.map(item => 
                 item.zaddr 
                     ? <ZaddrCard key={item.id} user={item} copied={copied} setCopied={setCopied} /> 
                     : null
-            )
+            )}
+            </>
             : <>
             <img id="spinner" src={logo} />
             <h2>Loading . . .</h2>
             </>
         }
-            <p className="dev-disclaimer">This site is still under development! If you encounter issues while testing, please let me know on <a target="_new" href="https://twitter.com/michaelharms70">Twitter</a> or <a target="_new" href="https://github.com/michaelharms6010/fe-zeitpages">Github.</a></p>
+            <p className="dev-disclaimer">This site is still under development! If you encounter issues while using it, please let me know on <a target="_new" href="https://twitter.com/michaelharms70">Twitter</a> or <a target="_new" href="https://github.com/michaelharms6010/fe-zeitpages">Github.</a></p>
         </div>
      )
 
