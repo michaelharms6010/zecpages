d7d0af04dd7f283aef343e1a12781e2b4a02e7e6
Michael Harms <michael.harms6010@gmail.com>
2020-02-19 11:24:48 -0600

    tweaks styling

--- src/components/Board.js
+++ src/components/Board.js
@@ -21,7 +21,7 @@ export default function Board() {
     return (
         <div className="z-board">
             <h2>Anonymous Memo Board</h2>
-            <h4>Send at least .001 ZEC to zs1n5m4szkmqup6ht9nuwke9j5w6pwcd527l4sm8u2aqqhaedjv5at64el6eyazm6engqplx0ht6x9 and it will be posted to this page for all to see!</h4>
+            <h4 className="instructions-header">Send at least .001 ZEC to zs1n5m4szkmqup6ht9nuwke9j5w6pwcd527l4sm8u2aqqhaedjv5at64el6eyazm6engqplx0ht6x9 and it will be posted to this page for all to see!</h4>
             {posts.map(item => 
                 <div className={item.amount > 1000000 ? "highlighted-board-post board-post" : "board-post"}>
                     <p className="post-text">{item.memo}</p>
