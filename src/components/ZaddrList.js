d4554da78e32e333256847e15b4c7f1780912a6c
Michael Harms <michael.harms6010@gmail.com>
2020-03-09 15:47:06 -0500

    adds filter checkboxes

--- src/components/ZaddrList.js
+++ src/components/ZaddrList.js
@@ -2,11 +2,18 @@ import React, {useState, useEffect, useContext} from "react";
 import {ZaddrContext} from "../contexts/ZaddrContext"
 import ZaddrCard from "./ZaddrCard";
 import logo from "../zcash-icon.png"
+import FilterBar from "./FilterBar"
 
 export default function ZaddrList (props) {
     const { zaddrs, copied, setCopied, loaded, setLoaded} = useContext(ZaddrContext);    
     const [results, setResults] = useState(zaddrs);
     const [search, setSearch] = useState("");
+    const [filters, setFilters] = useState({
+        needs_twitter: false,
+        needs_proof: false,
+        needs_website: false,
+        needs_email: false,
+    })
     
 
     useEffect( _ => {
@@ -32,6 +39,11 @@ export default function ZaddrList (props) {
 
     }, [search, zaddrs])
 
+    const handleFilterChange = e => {
+        setFilters({...filters, [e.target.name]: e.target.checked})
+        console.log(filters)
+    }
+
     return(
         <div className="zaddr-list">
             <h2 className="main-header">Zcash Z-address Directory</h2>     
@@ -44,12 +56,29 @@ export default function ZaddrList (props) {
                 value={search}
                 onChange={e => setSearch(e.target.value)}
                 />
+                <div className="filter-checkboxes">
+                    <label>Twitter
+                    <input 
+                        type="checkbox" 
+                        name="needs_twitter"
+                        checked={filters.needs_twitter}
+                        onChange={handleFilterChange}
+                    /></label>
+                    <label>Proof
+                    <input 
+                        type="checkbox" 
+                        name="needs_proof"
+                        checked={filters.needs_proof}
+                        onChange={handleFilterChange}
+                    /></label>
+                </div>
                 {results.map(item => 
                     item.zaddr 
                         ? <ZaddrCard key={item.id} user={item} copied={copied} setCopied={setCopied} /> 
                         : null
                 )}
                 <p>This humble directory contains {zaddrs.filter(item => item.zaddr ).length} 🦓 people! Help us grow!</p>
+                <FilterBar results={results} setResults={setResults} />
             </>
             : 
             <>
