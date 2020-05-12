c3ff7f6fbf85e1651a7f378a9636836922e8afcc
Michael Harms <michael.harms6010@gmail.com>
2020-05-12 18:21:27 -0500

    adds labels to auth form

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -5,6 +5,7 @@ import ReactGA from "react-ga";
 
 export default function Login({history}) {
     const [formInfo, setFormInfo] = useState({username: "", password: "", password2: ""});
+    const [alert, setAlert] = useState("")
     const {setLoggedIn} = useContext(UserContext);
     const handleChange = e => {
         setFormInfo({...formInfo, [e.target.name]: e.target.value})
@@ -12,39 +13,49 @@ export default function Login({history}) {
 
     const handleSubmit = e => {
         e.preventDefault();
-        axios.post("https://be.zecpages.com/auth/register", formInfo)
-            .then(res => {
-                ReactGA.event({category: "User", action: `created account ${formInfo.username} `});
-                localStorage.setItem("jwt", res.data.token)
-                setLoggedIn(true)
-                history.push("/edit")
-            })
-            .catch(err => console.error(err))
+        if (formInfo.password !== formInfo.password2) {
+            setAlert("Passwords do not match");
+        } else {
+            setAlert("Passwords do not match");
+            axios.post("https://be.zecpages.com/auth/register", formInfo)
+                .then(res => {
+                        
+                        ReactGA.event({category: "User", action: `created account ${formInfo.username} `});
+                        localStorage.setItem("jwt", res.data.token)
+                        setLoggedIn(true)
+                        history.push("/edit")
+                    
+                })
+                .catch(err => console.error(err))
+        }
     }
     
     return (
     <div className="auth-form">
         <h2>Register an account to post your zaddr</h2>
-        <form onSubmit={handleSubmit}>   
+        <form onSubmit={handleSubmit}>
+            <label>Username  
             <input
             name="username"
             type="text"
             value={formInfo.username}
             onChange={handleChange}
-            placeholder="username" />
-            
+            /></label>
+           <label>Password
             <input
             name="password"
             type="password"
             value={formInfo.password}
             onChange={handleChange}
-            placeholder="password" />
+            /></label>
+            <label>Confirm Password 
             <input
             name="password2"
             type="password"
             value={formInfo.password2}
             onChange={handleChange}
-            placeholder="Confirm Password" />
+            /></label>
+            {alert ? <h5>{alert}</h5> : null}
             <button>Submit</button>
         </form> 
     </div>
