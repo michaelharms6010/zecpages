ef710023dc970d15c724d801995405e1118ac48c
Michael Harms <michael.harms6010@gmail.com>
2021-01-14 19:19:43 -0600

    fix for phones

--- src/components/Board.js
+++ src/components/Board.js
@@ -46,6 +46,7 @@ export default function Board() {
     const viewKey = "zxviews1q0duytgcqqqqpqre26wkl45gvwwwd706xw608hucmvfalr759ejwf7qshjf5r9aa7323zulvz6plhttp5mltqcgs9t039cx2d09mgq05ts63n8u35hyv6h9nc9ctqqtue2u7cer2mqegunuulq2luhq3ywjcz35yyljewa4mgkgjzyfwh6fr6jd0dzd44ghk0nxdv2hnv4j5nxfwv24rwdmgllhe0p8568sgqt9ckt02v2kxf5ahtql6s0ltjpkckw8gtymxtxuu9gcr0swvz"
 
     const reformatShields = str => {
+        if (!str.includes("🛡")) return str
         let output = []
         for (let i = 0; i < str.length ; i++) {
 
