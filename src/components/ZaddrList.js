61068d5e22e5352d95732998185b3413d09b8ca4
Michael Harms <michael.harms6010@gmail.com>
2021-01-17 20:55:19 -0600

    slick

--- src/components/ZaddrList.js
+++ src/components/ZaddrList.js
@@ -72,10 +72,10 @@ export default function ZaddrList (props) {
         <div className="zaddr-list">
             {/* <p className="dev-disclaimer">Zecpages is now running a lightwalletd node! Connect your light wallet to https://lightwalletd.zecpages.com:443 <br/> Or via cli with <code> ./zecwallet-cli.exe --server https://lightwalletd.zecpages.com:443</code></p> */}
             <h2 className="main-header">Zcash Z-address Directory</h2>
-            <div className="top-buttons">
+            {/* <div className="top-buttons">
                 <a className="export-button" href="https://be.zecpages.com/users" target="_new"><button>Export All User Zaddr Data</button></a>
                 <button className="export-button" onClick={handleCopyAll}>Copy all filtered zaddrs to clipboard (comma-separated)</button>
-            </div>
+            </div> */}
             {searching || zaddrs.length > 0
             ? 
             <> 
@@ -108,7 +108,7 @@ export default function ZaddrList (props) {
                     /></label>
                 </div>  
                 </div>
-                    {searching ? <p className="results-count">{zaddrs.length} results <span className="cancel-search-x" onClick={_=> setSearching(false)}>Clear Search</span></p> : loadingSearch ? <span>Searching...</span> : null }
+                    {searching ? <p className="results-count">{zaddrs.length} results<br/> <span className="cancel-search-x" onClick={_=> setSearching(false)}>Clear Search</span></p> : loadingSearch ? <span>Searching...</span> : null }
                 {searching 
                     ? null 
                     : <div className="zaddr-page-buttons">
