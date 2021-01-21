5b622adfc18efa27d0eb568bdc4d05d07f516561
Michael Harms <michael.harms6010@gmail.com>
2021-01-20 22:49:57 -0600

    adds canary

--- src/components/About.js
+++ src/components/About.js
@@ -39,6 +39,38 @@ export default function About() {
                 <p className="zaddr">zs1q4jdaenhv5pdn4uqn86qekt7j2dch66uuszyp05ywne8z7yh56dhqjekpqle72skfl8ycd86ew4</p>
                 <Link to="/noonervision" style={{textDecoration: "none"}}><h2 style={{margin: "1% auto", borderRadius: "8%", padding: "20px", width: "300px", color: "#ddd", background: "black"}}>Graphic Design by <ShieldIcon /><span style={{color: "#bec0fe" }}>noonervision</span><ShieldIcon /></h2></Link>
             </div>
+            <div style={{textAlign: 'left'}} className="about-content">
+               <code  className="canary">
+               -----BEGIN PGP SIGNED MESSAGE-----<br/>
+                Hash: SHA256<br/><br/>
+
+                As of January 20th, 2021, Zecpages.com has not received any National Security Letters or FISA court orders, and we have not been subject to any gag order(s) by a FISA court, or any other similar court(s) of any government. Zecpages has never placed any backdoors in our hardware or software and has not received any requests to do so. Zecpages has never disclosed any user communications to any third party. No searches or seizures of any kind have ever been performed on Zecpages assets.<br/><br/>
+
+                Recent headlines:<br/>
+                DHS pauses some deportations for 100 days - https://www.cnn.com/2021/01/20/politics/homeland-security-pause-some-deportations/index.html<br/><br/>
+
+                Amanda Gorman calls on Americans - https://www.cnn.com/2021/01/20/politics/amanda-gorman-inauguration-poem-trnd/index.html<br/><br/>
+
+                -----BEGIN PGP SIGNATURE-----<br/><br/>
+
+                iQEzBAEBCAAdFiEEHR4JGHTE2yF9R32s3x/m0fUOzv4FAmAJBgsACgkQ3x/m0fUO<br/>
+                zv496gf+Kk4A4pY1cDfPbSMVw67Bvmydc2TF/x/HqChKF1ehrtm4EEoWrWO8waTx<br/>
+                1QiCYtJ7T2llcqtqyTQRxumSX8iLnWfjxkD12lwUCX8EQy11GJ9PA+KmZ4dPFcap<br/>
+                Nn/dRv8gMaFhNTy1qrPncN/Le6k8L4Bgr0r1GAEbGz2MnBZgXL6tpa1YUtmT1j/o<br/>
+                iGgbnU4H4UEMobxbtgI15SezxZB+BCvMU/0Olf0sk7bOpGy0wfYjQL5u6LV05vL4<br/>
+                WF90FbpnGvvvTO1D9k2lo9VCaNfGvxht7YBBk76HvMckScFSeqOrwn5Hph0SHLZZ<br/>
+                ZeqtOLuDQbpvN1dhFCPCzpOGUWIXFQ==<br/>
+                =lRxJ<br/>
+                -----END PGP SIGNATURE-----<br/>
+
+               </code>
+               <hr />
+               <code>
+                gpg --keyserver keys.gnupg.net --recv-key F50ECEFE<br/><br/>
+                Fingerprint: 1D1E 0918 74C4 DB21 7D47  7DAC DF1F E6D1 F50E CEFE<br/><br/>
+                gpg --verify zecpages_canary_message.asc<br/><br/>
+               </code>
+            </div>
             <p>Your ip is {ip}. Think about that.</p>
          </div>
     </> 
