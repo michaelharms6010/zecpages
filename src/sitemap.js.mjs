96248a7f6a47d7b3c2204250f18d386d0059a3ad
Michael Harms <michael.harms6010@gmail.com>
2021-01-27 11:22:35 -0600

    sitemap more robust

--- src/sitemap.js.mjs
+++ src/sitemap.js.mjs
@@ -3,7 +3,21 @@ console.log('hi')
 import path from 'path'
 import sm from 'sitemap'
 import fs from 'fs'
+import axios from 'axios';
 import { Readable } from 'stream'
+const baseURL = "https://be.zecpages.com"
+
+let postIds, usernames;
+
+// get post ids
+// get usernames
+axios.get(baseURL + "/board/postids").then(r => {
+  console.log(r)
+  postIds = r.data.posts;
+
+axios.get(baseURL + "/users/getUsernames").then(r => {
+  console.log(r)
+  usernames = r.data.usernames;
 
 
 
@@ -14,8 +28,16 @@ const links = [{ url: '/',  changefreq: 'daily', priority: 1 },
 { url: '/directory',  changefreq: 'weekly',  priority: 0.5 },
 { url: '/about', changefreq: 'weekly',  priority: 0.5 }]
 
+postIds.forEach(postId => {
+  links.push({ url: `/board/post/${postId}`,  changefreq: 'weekly', priority: .4 })
+})
 
-fs.writeFile('newfile.txt', "", (err) => {
+usernames.forEach(postId => {
+  links.push({ url: `/${postId}`,  changefreq: 'weekly', priority: .3 })
+})
+
+
+fs.writeFile('public/sitemap.xml', "", (err) => {
   if (err) throw err;
 
   console.log("The file was succesfully saved!");
@@ -23,7 +45,10 @@ fs.writeFile('newfile.txt', "", (err) => {
 
 sm.streamToPromise(Readable.from(links).pipe(stream)).then((data) =>
 
-  fs.writeFileSync("build/sitemap.xml", data.toString())
+  fs.writeFileSync("public/sitemap.xml", data.toString())
 )
 
 console.log(`Sitemap written at ${OUTPUT_FILE}`)
+
+}).catch(err => console.log(err))
+}).catch(err => console.log(err))
