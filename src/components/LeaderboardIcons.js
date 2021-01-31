0c0001a11af4a909f9e2e4022926b473f7379f5c
Michael Harms <michael.harms6010@gmail.com>
2021-01-30 22:51:04 -0600

    leaderboard icons

---
+++ src/components/LeaderboardIcons.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function LeaderboardIcons() {
+    return(
+        <>
+            <div className="leaderboard-icon leaderboard-icon-one"></div>
+            <div className="leaderboard-icon leaderboard-icon-two">b</div>
+            <div className="leaderboard-icon leaderboard-icon-three">c</div>
+            <div className="leaderboard-icon leaderboard-icon-four">d</div>
+            <div className="leaderboard-icon leaderboard-icon-five">e</div>
+            <div className="leaderboard-icon leaderboard-icon-six"></div>
+            <div className="leaderboard-icon leaderboard-icon-seven"></div>
+            <div className="leaderboard-icon leaderboard-icon-eight"></div>
+            <div className="leaderboard-icon leaderboard-icon-nine"></div>
+            <div className="leaderboard-icon leaderboard-icon-ten"></div>
+        </>
+    )
+}
