b0db92b72aecdeb0ad905e56024779c3d4aef9c6
Michael Harms <michael.harms6010@gmail.com>
2021-02-13 07:19:43 -0600

    board link on posts

--- src/components/BoardPost.js
+++ src/components/BoardPost.js
@@ -157,6 +157,7 @@ export default function BoardPost(props) {
     <>
     {post.reply_to_post ? <Link className="replying-to-link" to={`/board/post/${post.reply_to_post}`}>← Replying to post {post.reply_to_post}</Link> : <Link  className="replying-to-link" to="/board">← Back to board</Link>}
     <div key={post.id} id={post.id === pinned.id ? "pinned-post" : ""} className={post.amount >= 10000000 ? "highlighted-board-post board-post individual-post" : "board-post individual-post"}>
+    {!!post.board_name && <p className="post-text sub-board-link">Posted to <Link className="z-link" to={`/z/${post.board_name}`}>z/{post.board_name}</Link></p>}
         <p className="post-text">{reformatShields(post.memo.split("â€™").join("'").replace(replyRegex, ""), post.reply_zaddr, post.username)}</p>
        
        <hr></hr>
