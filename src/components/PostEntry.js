85179d37000ce493892145c747554a0ba2970a41
Michael Harms <michael.harms6010@gmail.com>
2021-02-26 15:41:08 -0600

    dude

--- src/components/PostEntry.js
+++ src/components/PostEntry.js
@@ -16,7 +16,7 @@ export default function PostEntry({qrVal, replyBody, darkMode, formatReplyBody, 
         <div className="reply-editor">
         <div className="reply-text-editor">
             <h2>{isReply ? "Write reply:" : "Write a post:"}</h2>
-            <textarea maxlength="500" placeholder="Type your message, then scan the QR code from your wallet app." value={replyBody} onChange={e => { formatReplyBody(e.target.value) }} />
+            <textarea style={!isReply && darkMode ? {borderColor: "#ddd", color: "#eee", background:  "#000126"} : {} } maxlength="500" placeholder="Type your message, then scan the QR code from your wallet app." value={replyBody} onChange={e => { formatReplyBody(e.target.value) }} />
         </div>
         {/* #bec0fe #0a5e55*/}
         <QRCode bgColor={darkMode ? "#111111" : 'black'} fgColor={darkMode ? post.amount >= 10000000 ? "#C46274" : "#7377EF" : post.amount >= 10000000 ? "#ff879b" : '#bec0fe'} style={{display: 'inline-block', margin: '0 auto'}} includeMargin={true} size={256} value={`zcash:${qrVal}?amount=0.001&memo=${URLSafeBase64.encode(Buffer.from(`${isReply ? `REPLY::${post.id}` : ""}${replyBody}`))}`} />
