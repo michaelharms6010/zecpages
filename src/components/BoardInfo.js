49a128b100da6a42e396b71f6c899b0f9cd8e04f
Michael Harms <michael.harms6010@gmail.com>
2021-06-07 15:19:39 -0500

    quick scripts added

--- src/components/BoardInfo.js
+++ src/components/BoardInfo.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 export default function BoardInfo() {
     const [showViewKey, setShowViewKey] = React.useState(false)
+    const [fullNodeInstructions, setFullNodeInstructions] = React.useState(true)
     const viewKey = "zxviews1q0duytgcqqqqpqre26wkl45gvwwwd706xw608hucmvfalr759ejwf7qshjf5r9aa7323zulvz6plhttp5mltqcgs9t039cx2d09mgq05ts63n8u35hyv6h9nc9ctqqtue2u7cer2mqegunuulq2luhq3ywjcz35yyljewa4mgkgjzyfwh6fr6jd0dzd44ghk0nxdv2hnv4j5nxfwv24rwdmgllhe0p8568sgqt9ckt02v2kxf5ahtql6s0ltjpkckw8gtymxtxuu9gcr0swvz"
-
+    const boardZaddr = "zs1j29m7zdhhyy2eqrz89l4zhk0angqjh368gqkj2vgdyqmeuultteny36n3qsm47zn8du5sw3ts7f"
     return(
         <div className="z-board">
             <div className='zaddr-card board-info'>
@@ -21,6 +22,25 @@ export default function BoardInfo() {
                 {viewKey}
                 <a className="view-key-link" style={{margin: "1%", display: "block", textDecoration: "underline"}} target="_blank" rel="noopener noreferrer" href="https://electriccoin.co/blog/explaining-viewing-keys/">
                     What's a viewing key?</a> 
+                    <button onClick={_ => setFullNodeInstructions(!fullNodeInstructions)}>{fullNodeInstructions ? "See light wallet instructions" : "See full node instructions"}</button>
+                {fullNodeInstructions 
+                ? <>
+                    <h2>How to duplicate/audit ZECpages (Full node):</h2>
+                    <h4>1. Import ZECpages key:</h4>
+                    <div className="code-snippet"><code>zcash-cli z_importviewingkey "{viewKey}" whenkeyisnew 419200</code></div>
+                    <br/>
+                    <h4>2. See raw post data:</h4>
+                    <div className="code-snippet"><code>zcash-cli z_listreceivedbyaddress "{boardZaddr}"</code></div>
+                  </>
+                : <>
+                    <h2>How to duplicate/audit ZECpages (light wallet):</h2>
+                    <h4>1. Import ZECpages key:</h4>
+                    <div className="code-snippet"><code>zecwallet-cli import {viewKey} 419200</code></div>
+                    <br/>
+                    <h4>2. See raw post data:</h4>
+                    <div className="code-snippet"><code>zecwallet-cli list</code></div>
+                  </>}
+                
             </p>}
             <button style={{marginBottom: "18px"}} onClick={_ => setShowViewKey(!showViewKey)} >{showViewKey ? "Hide View Key" : "Show View Key"}</button><br/>
 
