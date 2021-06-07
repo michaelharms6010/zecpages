59628db731cd2909938da1a646974a12fbe8f756
Michael Harms <michael.harms6010@gmail.com>
2021-06-07 16:56:21 -0500

    whatever

--- src/components/BoardInfo.js
+++ src/components/BoardInfo.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function BoardInfo() {
     const [showViewKey, setShowViewKey] = React.useState(false)
-    const [fullNodeInstructions, setFullNodeInstructions] = React.useState(true)
+    const [fullNodeInstructions, setFullNodeInstructions] = React.useState(false)
     const viewKey = "zxviews1q0duytgcqqqqpqre26wkl45gvwwwd706xw608hucmvfalr759ejwf7qshjf5r9aa7323zulvz6plhttp5mltqcgs9t039cx2d09mgq05ts63n8u35hyv6h9nc9ctqqtue2u7cer2mqegunuulq2luhq3ywjcz35yyljewa4mgkgjzyfwh6fr6jd0dzd44ghk0nxdv2hnv4j5nxfwv24rwdmgllhe0p8568sgqt9ckt02v2kxf5ahtql6s0ltjpkckw8gtymxtxuu9gcr0swvz"
     const boardZaddr = "zs1j29m7zdhhyy2eqrz89l4zhk0angqjh368gqkj2vgdyqmeuultteny36n3qsm47zn8du5sw3ts7f"
     return(
@@ -38,7 +38,32 @@ export default function BoardInfo() {
                     <div className="code-snippet"><code>zecwallet-cli import {viewKey} 419200</code></div>
                     <br/>
                     <h4>2. See raw post data:</h4>
-                    <div className="code-snippet"><code>zecwallet-cli list</code></div>
+                    <div className="code-snippet"><code>zecwallet-cli list</code></div><br/>
+                    <h4 style={{wordBreak: 'normal'}}>3. (Audit) Run this python script to see filtered posts (some of these are below payment threshold, some have no printable content, some are my own that I got insecure about heh, and some are just plain nasty):</h4>
+                    <div style={{wordBreak: 'normal', paddingTop: 20, padding: 10, borderRadius: 50, textAlign: 'left'}} className="python-script">
+                        <code>
+                        # zecpages_audit.py<br/>
+                        import sys, json<br/>
+                        import requests<br/>
+                        import re<br/>
+
+                        txs = json.load(sys.stdin)<br/>
+                        r = requests.get('https://be.zecpages.com/board/txids')<br/>
+
+                        savedTxids = map(lambda tx: re.sub(r'-\d+', "", tx['txid']), r.json())<br/>
+
+                        print(savedTxids)<br/>
+
+                        txs = filter(lambda tx: tx.get("memo") and tx['amount'] > 0 and tx['txid'] not in savedTxids and not re.search('like:', tx['memo'], re.IGNORECASE) and not re.search('SUBSCRIBE:', tx['memo'], re.IGNORECASE) and 'TElLRT' not in tx['memo'], txs)<br/>
+
+                        print(txs)<br/>
+                        
+                    </code>
+                    <hr/>
+                    Run it thusly:<br/> 
+                        <code>zecwallet-cli list | python zecpages_audit.py</code>
+                    <br/>
+                    </div>
                   </>}
                 
             </p>}
