d1a721f70a6e32cd18979378d96bbc2adf47ed1e
Michael Harms <michael.harms6010@gmail.com>
2023-07-06 16:29:32 -0500

    nice

--- src/components/widgets/HalvingCountdown.js
+++ src/components/widgets/HalvingCountdown.js
@@ -8,6 +8,13 @@ export default function HalvingCountdown({role, location}) {
     const search = location.search;
     const params = new URLSearchParams(search);
     const bgColor = params.get('bgcolor');
+    const body = {
+        "id": "blockNumber",
+        "jsonrpc": "2.0",
+        "method": "getblockchaininfo"
+        }
+
+    
     const [styles, setStyles] = useState({foreground: {}, background: {background: bgColor || 'white'}})
     const [windowHeight, setWindowHeight] = useState(`${window.innerHeight}px`)
     const [loading, setLoading] = useState(true)
@@ -38,8 +45,8 @@ export default function HalvingCountdown({role, location}) {
         
     },[])
     
-    const [blockHeight, setBlockHeight] = useState(2146585)
-    const [secondsToHalving, setSecondsToHalving] = useState(((new Date("2024-11-15")).getTime() - Date.now() + (3600000 * 6)) / 1000)
+    const [blockHeight, setBlockHeight] = useState(null)
+    const [secondsToHalving, setSecondsToHalving] = useState()
     const halvingBlock = 2726400
 
     const renderer = ({ days, hours, minutes, seconds, completed }) => {
@@ -68,32 +75,35 @@ export default function HalvingCountdown({role, location}) {
     }
 
 
+
     useEffect(_ => {
-        // axios.get("https://zecblockexplorer.com/api/")
-        //     .then(({data}) => {
-        //         console.log(data)
-        //             setBlockHeight(+data.blockbook.bestHeight)
-                
-        //     })
-        //     .catch(console.error)
+        axios.post("https://zec.getblock.io/62c2e4dd-3012-4127-91cf-70ab813bd22b/mainnet/", body)
+            .then(({data}) => {
+                console.log(data)
+                setBlockHeight(+data.result.blocks)
+                setLoading(false)
+            })
+            .catch(e => setLoading(false))
     },[])
 
     useEffect(_ => {
-        if (false && blockHeight) {
+        if (blockHeight) {
             setSecondsToHalving((halvingBlock - blockHeight) * 75)
+        } else {
+            setSecondsToHalving(((new Date("2024-11-15")).getTime() - Date.now() + (3600000 * 6)) / 1000)
         }
-        setTimeout(_ => setLoading(false), 50)
+        
     }, [blockHeight])
 
     return(
         <div style={{minHeight: windowHeight, ...styles.background}} className={`${!!styles.vertical && "vertical"} halving-countdown-widget`}>
-            {!loading && blockHeight && <div style={{width: "100%"}}>
+            {!loading && <div style={{width: "100%"}}>
 
             <img style={styles.logo} height={200} width={173} src={logo}></img>
             <div style={styles.foreground} className="countdown-header">
                 <h2 style={styles.foreground}>Zcash's next halving will take place at  <br/>~{new Date(Date.now() + secondsToHalving * 1000).toLocaleString()}</h2>
                 <h2 style={styles.foreground}>Halving block: {halvingBlock}</h2>            
-                {false && !!blockHeight && <h2 style={styles.foreground}>Current block: {blockHeight} ({halvingBlock - blockHeight} blocks to go)</h2>}
+                {!!blockHeight && <h2 style={styles.foreground}>Current block: {blockHeight} ({halvingBlock - blockHeight} blocks to go)</h2>}
             </div>
             {!!secondsToHalving && 
                 <Countdown
